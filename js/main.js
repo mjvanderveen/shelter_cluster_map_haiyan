098e750686330ac2210f87406b00bc5f65113223
Maarten <forward@mvanderveen.nl>
2014-03-05 17:04:23 +0100

    new layers

--- js/main.js
+++ js/main.js
@@ -2,54 +2,41 @@
 
 var base_hotosm = L.tileLayer(
         'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',{
-        attribution: '&copy; OpenStreetMap contributors, <a href="http://hot.openstreetmap.org/">Humanitarian OpenStreetMap Team</a>'}
+        attribution: ''}
 );
 
 var base_osm = L.tileLayer(
         'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-        attribution: '&copy; OpenStreetMap contributors'}
+        attribution: ''}
 );
 
 //two needs layers created
 
-var overlay_hhdamaged = L.mapbox.tileLayer('brcmaps.gm59izfr');
+var overlay_zi = L.mapbox.tileLayer('brcmaps.sr307ldi');
 
-var overlay_damagedensity = L.mapbox.tileLayer('brcmaps.y9esif6r');
-
-var overlay_es_agency_count = L.mapbox.tileLayer('brcmaps.w8aug14i');
-
-var overlay_ssr_agency_count = L.mapbox.tileLayer('brcmaps.uujo47vi');
-
-var overlay_es_planned = L.mapbox.tileLayer('brcmaps.dg4d9529');
-
-var overlay_es_reached = L.mapbox.tileLayer('brcmaps.l0v34n29');
-
-var overlay_ssr_planned = L.mapbox.tileLayer('brcmaps.ma9n3ik9');
-
-var overlay_ssr_reached = L.mapbox.tileLayer('brcmaps.slri19k9');
 
 // two 3w layers created
 // these consist of teasers and basemap of boundaries
 // leaflet layer groups used to combine these
 
-var es3w = new L.LayerGroup(),
-    ssr3w = new L.LayerGroup();
+//var es3w = new L.LayerGroup(),
+//    ssr3w = new L.LayerGroup();
 
 
-var overlay_es3w = L.mapbox.tileLayer('brcmaps.n5wpcik9').addTo(es3w);
+//var overlay_es3w = L.mapbox.tileLayer('brcmaps.n5wpcik9').addTo(es3w);
 
-var overlay_ssr3w = L.mapbox.tileLayer('brcmaps.nuoqd7vi').addTo(ssr3w);
+//var overlay_ssr3w = L.mapbox.tileLayer('brcmaps.nuoqd7vi').addTo(ssr3w);
 
-var grid_overlay_es3w = L.mapbox.gridLayer('brcmaps.n5wpcik9').addTo(es3w);
+//var grid_overlay_es3w = L.mapbox.gridLayer('brcmaps.n5wpcik9').addTo(es3w);
 
-var grid_overlay_ssr3w = L.mapbox.gridLayer('brcmaps.nuoqd7vi').addTo(ssr3w);
+//var grid_overlay_ssr3w = L.mapbox.gridLayer('brcmaps.nuoqd7vi').addTo(ssr3w);
 
 // map initialised
 
 var map = L.map('map', {
-    center: [11.3, 123],
-    zoom: 8,
-    layers: [base_hotosm]
+    center: [52.3039761, 5.7735948],
+    zoom: 7,
+    layers: [base_hotosm, overlay_zi]
 });
 
 // layer control added
@@ -58,22 +45,12 @@ L.control.layers({
     'HOT OSM':base_hotosm,
     'OSM':base_osm
 }, {
-    'Household Damage': overlay_hhdamaged,
-    'Damage Density':overlay_damagedensity,
-    'Emergency Shelter - Agency Numbers':overlay_es_agency_count,
-    'Supported Self Recovery - Agency Numbers':overlay_ssr_agency_count,
-    'Emergency Support Planned':overlay_es_planned,
-    'Emergency Support Reached':overlay_es_reached,
-    'Supported Self Recovery Planned':overlay_ssr_planned,
-    'Supported Self Recovery Reached':overlay_ssr_reached,
-    'Emergency Shelter - Agency Names':es3w,
-    'Supported Self Recovery - Agency Names':ssr3w
+    'Deelnemende instellingen aan onderzoek': overlay_zi
 }).addTo(map);
 
 // legend control added
-
 var legendControl = L.mapbox.legendControl({position: 'bottomleft'}).addTo(map);
-
+	
 // listeners added for adding and removing layers
 
 map.on('layeradd', function(e) {
